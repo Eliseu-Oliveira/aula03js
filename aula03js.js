bf1eb5fcd54c47ccfa30a67c382577a09c2d0514
Eliseu1994 <eliseugermano@hotmail.com>
2023-02-06 17:02:49 -0300

    Aula 03 JS

---
+++ aula03js.js
@@ -0,0 +1,17 @@
+      // a. Criar uma variável com a string "Manipulando strings com JS"
+      let str1 = "Manipulando strings com JS";
+
+      // b. Exibir o conteúdo da variável em maiúsculo em um alerta
+      alert(str1.toUpperCase());
+
+      // c. Exibir os 5 primeiros caracteres da string no console
+      console.log(str1.substr(0, 5));
+
+      // d. Exibir a quantidade de caracteres da string em um alerta
+      alert("Quantidade de caracteres: " + str1.length);
+
+      // e. Criar uma variável com a string "Vamos concatenar as duas strings"
+      let str2 = "Vamos concatenar as duas strings";
+
+      // f. Concatenar as duas strings e exibir no console
+      console.log(str1 + " " + str2);
